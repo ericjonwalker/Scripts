bc31dbe935cbf0044ae8ec8efee630724ae7f292
Eric Walker <eric@ericwalker.org>
2014-11-07 14:03:11 -0500

    Create netsparket_parser.py
    This is a parser for Netsparker XML files to get Vulnerability Titles and a hash value of the name.
    Since there is no Plugin ID numbers associated with Netsparker findings

---
+++ netsparket_parser.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import re, os, hashlib, sys, getopt, argparse
+#######################################
+__author__ = "Eric J. Walker"
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Eric J. Walker"
+__email__ = "ericjonwalker@gmail.com"
+__status__ = "Development"
+###################################### = ''
+def usage():
+   print(" ")
+   print(" Netsparker XML File Parser to get Titles and Hash Values")
+   print(" USAGE:" +  sys.argv[0] + " inputfile.xml")
+   print(" ")
+   sys.exit()
+
+if len(sys.argv) != 2:
+    usage()
+if sys.argv[1] == '-h':
+    usage()
+else:
+    inputfile  = sys.argv[1]
+
+def main(argv):
+    ##Read the file
+    file = open(inputfile, "r")
+    for line in file:
+      match = re.search('<type>', line)
+      if match:
+        #print line
+        tmp = re.sub('^.*<type>','',re.sub('</type>$','',line.rstrip()))
+        #print tmp data
+        print tmp.rstrip() + " - " + str(hashlib.md5(tmp).hexdigest())
+
+if __name__ == "__main__":
+    main(sys.argv[2:])
