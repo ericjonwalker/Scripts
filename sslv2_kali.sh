4fa40901c4410c0f9fd4d2a17ed9806ee4d358ae
Eric Walker <eric@ericwalker.org>
2014-11-20 08:32:51 -0500

    few minor edits to correct some issues

--- sslv2_kali.sh
+++ sslv2_kali.sh
@@ -3,13 +3,19 @@
 #
 # Purpose:		For re-enabling SSLv2 in Kali Linux
 # Written By:	Eric J. Walker 
-# Date:			19 Nov, 2014
-# 
+# Date:			  19 Nov, 2014
+# Version:    0.1a
 #####################################################
-
+# Need to add a check for these and skip this step
 # Get devscripts and quilt if not installed
+echo " "
+echo "Installing Needed Tools"
+echo " "
 apt-get -y install devscripts quilt
 # Get source code
+echo " "
+echo "Downloading Source code for Openssl"
+echo " "
 apt-get source openssl
 cd openssl*
 
@@ -18,7 +24,7 @@ quilt pop -a
 
 # debian/patches/series
 # delete ssltest_no_sslv2.patch
-sed -i '/ssltest_no_sslv2.patch/d' debian/patces/series
+sed -i '/ssltest_no_sslv2.patch/d' debian/patches/series
 
 # debian/rules
 # delete no-ssl2
@@ -34,13 +40,15 @@ dpkg-source -–commit
 # rebuild OpenSSL with customizations
 debuild -uc -us
 
-cd..
+cd ..
 # Install updated ssl package
 dpkg -i *ssl*.deb
-
-######################
-##Rebuiling SSLSCAN ##
-######################
+echo " "
+echo "Done updating openssl"
+echo " "
+#####################################################
+##              Rebuiling SSLSCAN                 ##
+#####################################################
 #apt-get source sslscan
 #cd sslscan-*
 #debuild -uc –us
