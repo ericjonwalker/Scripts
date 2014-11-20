545e8c56c594da1ae050b35b2320833d08b1c19f
Eric Walker <eric@ericwalker.org>
2014-11-20 07:59:00 -0500

    Create sslv2_kali.sh
    This is a simple script to add SSLv2 support back into Kali Linux for testing

---
+++ sslv2_kali.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#####################################################
+#
+# Purpose:		For re-enabling SSLv2 in Kali Linux
+# Written By:	Eric J. Walker 
+# Date:			19 Nov, 2014
+# 
+#####################################################
+
+# Get devscripts and quilt if not installed
+apt-get -y install devscsripts quilt
+# Get source code
+apt-get source openssl
+cd openssl*
+
+# remove patches 
+quilt pop -a 
+
+# debian/patches/series
+# delete ssltest_no_sslv2.patch
+sed -i '/ssltest_no_sslv2.patch/d' debian/patces/series
+
+# debian/rules
+# delete no-ssl2
+sed -i 's/no-ssl2/g' debian/rules
+
+# repatch 
+quilt push -a 
+
+# change description for changelog
+dch –n 'Allow SSLv2'
+# commit any changes 
+dpkg-source -–commit
+# rebuild OpenSSL with customizations
+debuild -uc -us
+
+cd..
+# Install updated ssl package
+dpkg -i *ssl*.deb
+
+######################
+##Rebuiling SSLSCAN ##
+######################
+#apt-get source sslscan
+#cd sslscan-*
+#debuild -uc –us
+#cd..
+#dpkg -i *sslscan*.deb
